f3485405274cb36828b4447424a6d9537d933b39
Emily Paige Brown <brown.emily.paige@gmail.com>
2018-01-04 19:56:12 +0000

    assignment 22

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -11,21 +11,19 @@ class TopicsController < ApplicationController
      @topic = Topic.new
     end  
     
-    
    def create
-     @topic = Topic.new
-     @topic.name = params[:topic][:name]
-     @topic.description = params[:topic][:description]
-     @topic.public = params[:topic][:public]
- 
-     if @topic.save
-       redirect_to @topic, notice: "Topic was saved successfully."
-     else
-       flash.now[:alert] = "Error creating topic. Please try again."
-       render :new
-     end
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
    end
-   
    
    def edit
      @topic = Topic.find(params[:id])
