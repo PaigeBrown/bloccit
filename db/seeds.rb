e8f92107f9838ab7fa267d6dd0d4bf0866db1648
Emily Paige Brown <brown.emily.paige@gmail.com>
2018-01-05 15:38:40 +0000

    checkpoint27

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,16 @@
 require 'random_data'
 
 
+5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
  15.times do
    Topic.create!(
      name:         RandomData.random_sentence,
@@ -11,6 +21,7 @@ require 'random_data'
 
  50.times do
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -36,8 +47,15 @@ require 'random_data'
  puts "#{Post.count}"
  Post.find_or_create_by(title: "The Unique One", body: "Uniqueness all over here", )
  puts "#{Post.count}"
+ 
+ user = User.first
+ user.update_attributes!(
+   email: 'brown.emily.paige@gmail.com', 
+   password: 'Thunder'
+ )
 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
