f572887233c8c39e9edba131b8fc72ebb35d3d5d
Emily Paige Brown <brown.emily.paige@gmail.com>
2018-01-05 21:36:13 +0000

    assignment 30

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,35 +1,39 @@
 class Post < ActiveRecord::Base
-    belongs_to :topic
-    belongs_to :user
+  belongs_to :topic
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
-    has_many :comments, dependent: :destroy
-    has_many :votes
-    
-    default_scope { order('rank DESC') }
-    scope :ordered_by_title, -> {order('title DESC')}
-    scope :ordered_by_reverse_created_at, -> {order('rank ASC')}
-   
-   validates :title, length: { minimum: 5 }, presence: true
-   validates :body, length: { minimum: 20 }, presence: true
-   validates :topic, presence: true
-   validates :user, presence: true
-   
-   
-   def up_votes
-     votes.where(value: 1).count
-   end
- 
-   def down_votes
-     votes.where(value: -1).count
-   end
- 
-   def points
-     votes.sum(:value)
-   end
-   
-    def update_rank
-     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
-     new_rank = points + age_in_days
-     update_attribute(:rank, new_rank)
-    end
+  after_create :create_vote
+
+  default_scope { order('rank DESC') }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+   user.votes.create(post: self, value: 1)
+  end
 end
